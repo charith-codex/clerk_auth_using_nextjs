778c0bc88c6331bbfe3f84cc0eb887a0f626029b
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-08-12 23:27:10 +0530

    basic structure setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+         
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="font-bold text-center mt-10">Home Page</div>
+  );
+}
